7be6fb4cb4e9e7a4d5051d7536c7d6118efecb0c
KirkChen <kirkchen@kkbox.com>
2017-10-28 09:41:52 +0800

    Add discounter interface and vipdiscounter

---
+++ ts/discounters/vipDiscounter.ts
@@ -0,0 +1,11 @@
+class VipDiscounter implements IDiscounter {
+    Calculate(price: number, qty: number): number {
+        let totalPrice = price * qty;
+
+        if(totalPrice >= 500){
+            return totalPrice * 0.8;
+        }
+
+        return totalPrice;
+    }
+}
