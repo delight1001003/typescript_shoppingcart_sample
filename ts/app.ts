c29147202ed782ddc1c8ab57423c7956500513df
KirkChen <kirkchen@kkbox.com>
2017-10-28 09:38:51 +0800

    Add page script to get input value

---
+++ ts/app.ts
@@ -0,0 +1,9 @@
+/// <reference path="../node_modules/@types/jquery/index.d.ts" />
+
+$(document).ready(() => {
+    $('#calculate').on('click', () => {
+        let price = $('#price').val();
+        let qty = $('#qty').val();
+        let level = $('#level').val();
+    })
+});
