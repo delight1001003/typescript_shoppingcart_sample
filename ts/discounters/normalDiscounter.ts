3a8148ce7a80e02a56db95a73c4ff04a658df33a
KirkChen <kirkchen@kkbox.com>
2017-10-28 09:42:53 +0800

    Add normal discounter

---
+++ ts/discounters/normalDiscounter.ts
@@ -0,0 +1,11 @@
+class NormalDiscounter implements IDiscounter {
+    Calculate(price: number, qty: number): number {
+        let totalPrice = price * qty;
+
+        if (totalPrice >= 1000 && qty > 3) {
+            return totalPrice * 0.85;
+        }
+
+        return totalPrice;
+    }
+}
